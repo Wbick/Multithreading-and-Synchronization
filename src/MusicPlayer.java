e16bf04440f58c6a09e71535a7ab9cda586d79b8
William Bickerton <wbick096@uottawa.ca>
2025-11-25 14:23:24 -0500

    making files

---
+++ src/MusicPlayer.java
@@ -0,0 +1,20 @@
+class Thread1 extends Thread{
+    @Override
+    public void run(){
+        //do, mi, sol, si, do-octave
+    }
+}
+
+class Thread2 extends Thread{
+    @Override
+    public void run(){
+        //re, fa, la, do-octave
+    }
+}
+
+public class MusicPlayer{
+
+    public void main(String[] args){
+
+    }
+}
