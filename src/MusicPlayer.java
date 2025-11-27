26d0e0c2470744c85a926c48b09017c316901f0a
William Bickerton <wbick096@uottawa.ca>
2025-11-26 21:16:04 -0500

    updates working version

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,5 +1,3 @@
-import java.io.File;
-
 public class MusicPlayer{
     private static int allowPlay = 1;
     private static String musicSheet;
@@ -13,35 +11,44 @@ public class MusicPlayer{
         public void run(){
             String[] instructions = musicSheet.split(" ");
             for(String note : instructions){
-                //delay star of note
-                try {
-                    sleep(300);
-                } catch (InterruptedException e){}
-                //switch to other thread for those notes
                 if(note.equals("re") ||
                     note.equals("fa")||
-                    note.equals("la")){
+                    note.equals("la"))
+                    {
+                    System.out.println("Switching to thread 2");
+                    synchronized(this){
                         allowPlay = 2;
+                    }
+                    continue;
+                    
                 }else if(note.equals("do-octave"))allowPlay = 0;
 
-                //playing sound
-                if(allowPlay == 0 || allowPlay == 1){
-                    System.out.println("Thread1: "+note);
-                    sound.play("sounds/"+note+".wav");
-                }else{
-                    while(allowPlay == 2){
-                        try {
-                            Thread.sleep(300);
-                        } catch (InterruptedException e) {
-                        }
-                        continue;
-                    }
-                }
-                
-            }
-            
-        }
+                //delayes unitl threads turn
+                while(allowPlay == 2){System.out.print("");}
 
+                //normal notes case
+                if(allowPlay == 1){
+                    //delay note sound
+                    try {
+                        sleep(500);
+                        sound.play("sounds/"+note+".wav");
+                        System.out.println("Thread1: "+note);
+                    } catch (Exception e) {}
+
+
+                //do-octave case
+                }else if(allowPlay == 0){
+                    synchronized(this){
+                        try {
+                            sleep(500);
+                        } catch (InterruptedException e) {}
+                        sound.play("sounds/"+note+".wav");
+                        System.out.println("Thread1: "+note);
+                        break;
+                    }   
+                }
+            }
+        }
 
     } 
 
@@ -52,34 +59,43 @@ public class MusicPlayer{
         public void run(){
             String[] instructions = musicSheet.split(" ");
             for(String note : instructions){
-
-                //delay the start sound of note
-                try {
-                    sleep(300);
-                } catch (InterruptedException e){}
-
-                //switch to other thread for those notes
                 if(note.equals("do") ||
                     note.equals("mi")||
                     note.equals("sol")||
-                    note.equals("si")){
+                    note.equals("si"))
+                    {
+                    System.out.println("Switching to thread 1");
+                    synchronized(this){
                         allowPlay = 1;
-                }else if(note.equals("do-octave"))allowPlay = 0;
-
-                //playing sound
-                if(allowPlay == 0 || allowPlay == 2){
-                    System.out.println("Thread2: "+note);
-                    sound.play("sounds/"+note+".wav");
-                }else{
-                    while(allowPlay == 1){
-                        try {
-                            Thread.sleep(300);
-                        } catch (InterruptedException e) {
-                        }
                     }
                     continue;
+                    
+                }else if(note.equals("do-octave"))allowPlay = 0;
+
+                //delayes unitl threads turn
+                while(allowPlay == 1){System.out.print("");}
+
+                //normal notes case
+                if(allowPlay == 2){
+                    //delay note sound
+                    try {
+                        sleep(500);
+                        sound.play("sounds/"+note+".wav");
+                        System.out.println("Thread2: "+note);
+                    } catch (Exception e) {}
+
+
+                //do-octave case
+                }else if(allowPlay == 0){
+                    synchronized(this){
+                        try {
+                            sleep(500);
+                        } catch (InterruptedException e) {}
+                        sound.play("sounds/"+note+".wav");
+                        System.out.println("Thread2: "+note);
+                        break;
+                    }                    
                 }
-                
             }
         }
     }
@@ -89,12 +105,13 @@ public class MusicPlayer{
         Thread1 t1 = new Thread1();
         Thread2 t2 = new Thread2();
 
+        //musicSheet = "do do sol sol la la sol fa fa mi mi re re do sol sol fa fa mi mi re sol sol fa fa mi mi re do do sol sol la la sol fa fa mi mi re re do";
         musicSheet = "do re mi fa sol la si do-octave";
-
         allowPlay = 1;
 
         t1.start();
         t2.start();
-
+        
+        
     }
 }
