0c293aa963652ddcaa462919659b820aa37b5b57
jcaro033 <jcaro033@uottawa.ca>
2025-11-26 21:08:29 -0500

    changed code for player

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -3,7 +3,8 @@ import java.io.File;
 public class MusicPlayer{
     private static int allowPlay = 1;
     private static String musicSheet;
-    
+    private static boolean skipNote = false; 
+    private static boolean play = true;
     static FilePlayer sound = new FilePlayer();
 
     //do, mi, sol, si, do-octave
@@ -13,37 +14,35 @@ public class MusicPlayer{
         public void run(){
             String[] instructions = musicSheet.split(" ");
             for(String note : instructions){
-                //delay star of note
+                //delay start of note
                 try {
-                    sleep(300);
+                    sleep(600);
                 } catch (InterruptedException e){}
                 //switch to other thread for those notes
                 if(note.equals("re") ||
                     note.equals("fa")||
                     note.equals("la")){
+                        skipNote=false;
                         allowPlay = 2;
                 }else if(note.equals("do-octave"))allowPlay = 0;
-
+                else{
+                    allowPlay =1;
+                    skipNote = true;
+                }
                 //playing sound
                 if(allowPlay == 0 || allowPlay == 1){
                     System.out.println("Thread1: "+note);
                     sound.play("sounds/"+note+".wav");
-                }else{
-                    while(allowPlay == 2){
-                        try {
-                            Thread.sleep(300);
-                        } catch (InterruptedException e) {
-                        }
-                        continue;
-                    }
                 }
-                
+                    
+                }
+                play = false;
             }
             
         }
 
 
-    } 
+    
 
     //re, fa, la, do-octave
     public static class Thread2 extends Thread{
@@ -51,36 +50,27 @@ public class MusicPlayer{
         @Override
         public void run(){
             String[] instructions = musicSheet.split(" ");
-            for(String note : instructions){
-
-                //delay the start sound of note
-                try {
-                    sleep(300);
-                } catch (InterruptedException e){}
-
-                //switch to other thread for those notes
-                if(note.equals("do") ||
-                    note.equals("mi")||
-                    note.equals("sol")||
-                    note.equals("si")){
-                        allowPlay = 1;
-                }else if(note.equals("do-octave"))allowPlay = 0;
-
-                //playing sound
+            int noteNumber =-1;
+            //this thread is contantly looping
+            while(play) {
+            try {
+                sleep(1);
+                //playinf sound
+            } catch (InterruptedException e) {
+            }
                 if(allowPlay == 0 || allowPlay == 2){
+                    String note = instructions[++noteNumber];
                     System.out.println("Thread2: "+note);
                     sound.play("sounds/"+note+".wav");
-                }else{
-                    while(allowPlay == 1){
-                        try {
-                            Thread.sleep(300);
-                        } catch (InterruptedException e) {
-                        }
-                    }
-                    continue;
+                    allowPlay = -1;
+            }
+                //Thread 1 plays a note
+                else if(skipNote) {
+                    ++noteNumber;
+                    skipNote = false;
                 }
                 
-            }
+        }
         }
     }
 
